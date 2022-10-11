e96023d8d3ed9fec722b27182645693913fd0330
2088826 <LE2088826@crc-lennox.qc.ca>
2022-10-11 10:14:57 -0400

    FIXED: Properly implemented IMeasurable into Person class.
    Uncommented the corresponding test methods in PersonTestsStarterForm.

--- PersonTests/Person.cs
+++ PersonTests/Person.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Represents one person saving their name.
     /// </summary>
-    public class Person
+    public class Person : IMeasurable
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="Person"/> class.
